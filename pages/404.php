f17b9288a59f2ba8a448dc782ff82f84788d8145
fiokee <smleffiong@gmail.com>
2025-01-26 22:50:01 +0100

    fixing link issues

---
+++ pages/404.php
@@ -0,0 +1,16 @@
+<?php include 'includes/header.php'?>
+
+<section class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10 mx-auto text-center">
+          <img loading="lazy" decoding="async" src="images/404.png" alt="404" class="img-fluid mb-4" width="500" height="350">
+          <h1 class="mb-4">Page Not Found!</h1>
+          <a href="index.php" class="btn btn-outline-primary">Back To Home</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+
+<?php include 'includes/footer.php' ?>
--- pages/index.php
+++ pages/index.php
@@ -1,77 +1,6 @@
-<!DOCTYPE html>
+<?php include 'includes/header.php'?>
 
-<!--
- // WEBSITE: https://themefisher.com
- // TWITTER: https://twitter.com/themefisher
- // FACEBOOK: https://www.facebook.com/themefisher
- // GITHUB: https://github.com/themefisher/
--->
-
-<html lang="en-us">
-
-<head>
-	<meta charset="utf-8">
-	<title>Reporter - HTML Blog Template</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
-	<meta name="description" content="This is meta description">
-	<meta name="author" content="Themefisher">
-	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-	<link rel="icon" href="images/favicon.png" type="image/x-icon">
-  
-  <!-- theme meta -->
-  <meta name="theme-name" content="reporter" />
-
-	<!-- # Google Fonts -->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Neuton:wght@700&family=Work+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
-
-	<!-- # CSS Plugins -->
-	<link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
-
-	<!-- # Main Style Sheet -->
-	<link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-
-<header class="navigation">
-  <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light px-0">
-      <a class="navbar-brand order-1 py-0" href="index.php">
-        <img loading="prelaod" decoding="async" class="img-fluid" src="images/logo.png" alt="Reporter Hugo">
-      </a>
-      <div class="navbar-actions order-3 ml-0 ml-md-4">
-        <button aria-label="navbar toggler" class="navbar-toggler border-0" type="button" data-toggle="collapse"
-          data-target="#navigation"> <span class="navbar-toggler-icon"></span>
-        </button>
-      </div>
-      <form action="#!" class="search order-lg-3 order-md-2 order-3 ml-auto">
-        <input id="search-query" name="s" type="search" placeholder="Search..." autocomplete="off">
-      </form>
-      <div class="collapse navbar-collapse text-center order-lg-2 order-4" id="navigation">
-        <ul class="navbar-nav mx-auto mt-3 mt-lg-0">
-          <li class="nav-item"> <a class="nav-link" href="about.html">About Me</a>
-          </li>
-          <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button"
-              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Articles
-            </a>
-            <div class="dropdown-menu"> <a class="dropdown-item" href="travel.html">Travel</a>
-              <a class="dropdown-item" href="travel.html">Lifestyle</a>
-              <a class="dropdown-item" href="travel.html">Cruises</a>
-            </div>
-          </li>
-          <li class="nav-item"> <a class="nav-link" href="contact.html">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-</header>
-
-<main>
-  <section class="section">
+<section class="section">
     <div class="container">
       <div class="row no-gutters-lg">
         <div class="col-12">
@@ -81,7 +10,7 @@
           <div class="row">
             <div class="col-12 mb-4">
               <article class="card article-card">
-                <a href="article.html">
+                <a href="article.php">
                   <div class="card-image">
                     <div class="post-info"> <span class="text-uppercase">04 Jun 2021</span>
                       <span class="text-uppercase">3 minutes read</span>
@@ -95,7 +24,7 @@
                       <a href="#!">news</a>
                     </li>
                   </ul>
-                  <h2 class="h1"><a class="post-title" href="article.html">Is it Ethical to Travel Now?
+                  <h2 class="h1"><a class="post-title" href="article.php">Is it Ethical to Travel Now?
                       With that Freedom Comes Responsibility.</a></h2>
                   <p class="card-text">Heading Here is example of hedings. You can use this heading by following markdownify rules. For example: use # for heading 1 and use ###### for heading 6.</p>
                   <div class="content"> <a class="read-more-btn" href="article.html">Read Full Article</a>
@@ -105,7 +34,7 @@
             </div>
             <div class="col-md-6 mb-4">
               <article class="card article-card article-card-sm h-100">
-                <a href="article.html">
+                <a href="article.php">
                   <div class="card-image">
                     <div class="post-info"> <span class="text-uppercase">03 Jun 2021</span>
                       <span class="text-uppercase">2 minutes read</span>
@@ -118,17 +47,17 @@
                     <li> <a href="#!">travel</a>
                     </li>
                   </ul>
-                  <h2><a class="post-title" href="article.html">An
+                  <h2><a class="post-title" href="article.php">An
                       Experiential Guide to Explore This Kingdom</a></h2>
                   <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna …</p>
-                  <div class="content"> <a class="read-more-btn" href="article.html">Read Full Article</a>
+                  <div class="content"> <a class="read-more-btn" href="article.php">Read Full Article</a>
                   </div>
                 </div>
               </article>
             </div>
             <div class="col-md-6 mb-4">
               <article class="card article-card article-card-sm h-100">
-                <a href="article.html">
+                <a href="article.php">
                   <div class="card-image">
                     <div class="post-info"> <span class="text-uppercase">02 Jun 2021</span>
                       <span class="text-uppercase">2 minutes read</span>
@@ -141,7 +70,7 @@
                     <li> <a href="#!">lifestyle</a>
                     </li>
                   </ul>
-                  <h2><a class="post-title" href="article.html">What
+                  <h2><a class="post-title" href="article.php">What
                       to Do in Houston: Ideas for Your Visit</a></h2>
                   <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna …</p>
                   <div class="content"> <a class="read-more-btn" href="article.html">Read Full Article</a>
@@ -151,7 +80,7 @@
             </div>
             <div class="col-md-6 mb-4">
               <article class="card article-card article-card-sm h-100">
-                <a href="article.html">
+                <a href="article.php">
                   <div class="card-image">
                     <div class="post-info"> <span class="text-uppercase">01 Jun 2021</span>
                       <span class="text-uppercase">3 minutes read</span>
@@ -165,7 +94,7 @@
                       <a href="#!">news</a>
                     </li>
                   </ul>
-                  <h2><a class="post-title" href="article.html">Alaska
+                  <h2><a class="post-title" href="article.php">Alaska
                       Cruises Will Restart as Biden Signs New Tourism Law</a></h2>
                   <p class="card-text">Heading Here is example of hedings. You can use this heading by following markdownify rules. For example: use # for …</p>
                   <div class="content"> <a class="read-more-btn" href="article.html">Read Full Article</a>
@@ -175,7 +104,7 @@
             </div>
             <div class="col-md-6 mb-4">
               <article class="card article-card article-card-sm h-100">
-                <a href="article.html">
+                <a href="article.php">
                   <div class="card-image">
                     <div class="post-info"> <span class="text-uppercase">30 May 2021</span>
                       <span class="text-uppercase">3 minutes read</span>
@@ -189,17 +118,17 @@
                       <a href="#!">news</a>
                     </li>
                   </ul>
-                  <h2><a class="post-title" href="article.html">U.S.
+                  <h2><a class="post-title" href="article.php">U.S.
                       Travel Association Calls on CDC to Remove Cruise Restrictions</a></h2>
                   <p class="card-text">Heading Here is example of hedings. You can use this heading by following markdownify rules. For example: use # for …</p>
-                  <div class="content"> <a class="read-more-btn" href="article.html">Read Full Article</a>
+                  <div class="content"> <a class="read-more-btn" href="article.php">Read Full Article</a>
                   </div>
                 </div>
               </article>
             </div>
             <div class="col-md-6 mb-4">
               <article class="card article-card article-card-sm h-100">
-                <a href="article.html">
+                <a href="article.php">
                   <div class="card-image">
                     <div class="post-info"> <span class="text-uppercase">29 May 2021</span>
                       <span class="text-uppercase">3 minutes read</span>
@@ -212,17 +141,17 @@
                     <li> <a href="#!">destination</a>
                     </li>
                   </ul>
-                  <h2><a class="post-title" href="article.html">Top 7 Reasons to
+                  <h2><a class="post-title" href="article.php">Top 7 Reasons to
                       Visit Denver This Summer</a></h2>
                   <p class="card-text">Heading Here is example of hedings. You can use this heading by following markdownify rules. For example: use # for …</p>
-                  <div class="content"> <a class="read-more-btn" href="article.html">Read Full Article</a>
+                  <div class="content"> <a class="read-more-btn" href="article.php">Read Full Article</a>
                   </div>
                 </div>
               </article>
             </div>
             <div class="col-md-6 mb-4">
               <article class="card article-card article-card-sm h-100">
-                <a href="article.html">
+                <a href="article.php">
                   <div class="card-image">
                     <div class="post-info"> <span class="text-uppercase">28 May 2021</span>
                       <span class="text-uppercase">2 minutes read</span>
@@ -236,10 +165,10 @@
                       <a href="#!">tips</a>
                     </li>
                   </ul>
-                  <h2><a class="post-title" href="article.html">An Experiential
+                  <h2><a class="post-title" href="article.php">An Experiential
                       Guide to Explore This Kingdom</a></h2>
                   <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna …</p>
-                  <div class="content"> <a class="read-more-btn" href="article.html">Read Full Article</a>
+                  <div class="content"> <a class="read-more-btn" href="article.php">Read Full Article</a>
                   </div>
                 </div>
               </article>
@@ -258,7 +187,7 @@
                             </svg>
                           </a>
                         </li>
-                        <li class="page-item active "> <a href="index.html" class="page-link">
+                        <li class="page-item active "> <a href="index.php" class="page-link">
                             1
                           </a>
                         </li>
@@ -289,7 +218,7 @@
           <div class="widget-body">
             <img loading="lazy" decoding="async" src="images/author.jpg" alt="About Me" class="w-100 author-thumb-sm d-block">
             <h2 class="widget-title my-3">Hootan Safiyari</h2>
-            <p class="mb-3 pb-2">Hello, I’m Hootan Safiyari. A Content writter, Developer and Story teller. Working as a Content writter at CoolTech Agency. Quam nihil …</p> <a href="about.html" class="btn btn-sm btn-outline-primary">Know
+            <p class="mb-3 pb-2">Hello, I’m Hootan Safiyari. A Content writter, Developer and Story teller. Working as a Content writter at CoolTech Agency. Quam nihil …</p> <a href="about.php" class="btn btn-sm btn-outline-primary">Know
               More</a>
           </div>
         </div>
@@ -307,34 +236,34 @@
                 </div>
                 <div class="card-body px-0 pb-1">
                   <h3><a class="post-title post-title-sm"
-                      href="article.html">Portugal and France Now
+                      href="article.php">Portugal and France Now
                       Allow Unvaccinated Tourists</a></h3>
                   <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor …</p>
-                  <div class="content"> <a class="read-more-btn" href="article.html">Read Full Article</a>
+                  <div class="content"> <a class="read-more-btn" href="article.php">Read Full Article</a>
                   </div>
                 </div>
               </article>
-              <a class="media align-items-center" href="article.html">
+              <a class="media align-items-center" href="article.php">
                 <img loading="lazy" decoding="async" src="images/post/post-2.jpg" alt="Post Thumbnail" class="w-100">
                 <div class="media-body ml-3">
                   <h3 style="margin-top:-5px">These Are Making It Easier To Visit</h3>
                   <p class="mb-0 small">Heading Here is example of hedings. You can use …</p>
                 </div>
               </a>
-              <a class="media align-items-center" href="article.html"> <span class="image-fallback image-fallback-xs">No Image Specified</span>
+              <a class="media align-items-center" href="article.php"> <span class="image-fallback image-fallback-xs">No Image Specified</span>
                 <div class="media-body ml-3">
                   <h3 style="margin-top:-5px">No Image specified</h3>
                   <p class="mb-0 small">Lorem ipsum dolor sit amet, consectetur adipiscing …</p>
                 </div>
               </a>
-              <a class="media align-items-center" href="article.html">
+              <a class="media align-items-center" href="article.php">
                 <img loading="lazy" decoding="async" src="images/post/post-5.jpg" alt="Post Thumbnail" class="w-100">
                 <div class="media-body ml-3">
                   <h3 style="margin-top:-5px">Perfect For Fashion</h3>
                   <p class="mb-0 small">Lorem ipsum dolor sit amet, consectetur adipiscing …</p>
                 </div>
               </a>
-              <a class="media align-items-center" href="article.html">
+              <a class="media align-items-center" href="article.php">
                 <img loading="lazy" decoding="async" src="images/post/post-9.jpg" alt="Post Thumbnail" class="w-100">
                 <div class="media-body ml-3">
                   <h3 style="margin-top:-5px">Record Utra Smooth Video</h3>
@@ -384,33 +313,4 @@
   </section>
 </main>
 
-<footer class="bg-dark mt-5">
-  <div class="container section">
-    <div class="row">
-      <div class="col-lg-10 mx-auto text-center">
-        <a class="d-inline-block mb-4 pb-2" href="index.php">
-          <img loading="prelaod" decoding="async" class="img-fluid" src="images/logo-white.png" alt="Reporter Hugo">
-        </a>
-        <ul class="p-0 d-flex navbar-footer mb-0 list-unstyled">
-          <li class="nav-item my-0"> <a class="nav-link" href="about.html">About</a></li>
-          <li class="nav-item my-0"> <a class="nav-link" href="article.html">Elements</a></li>
-          <li class="nav-item my-0"> <a class="nav-link" href="privacy-policy.html">Privacy Policy</a></li>
-          <li class="nav-item my-0"> <a class="nav-link" href="terms-conditions.html">Terms Conditions</a></li>
-          <li class="nav-item my-0"> <a class="nav-link" href="404.html">404 Page</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div class="copyright bg-dark content">Designed &amp; Developed By <a href="https://themefisher.com/">Themefisher</a></div>
-</footer>
-
-
-<!-- # JS Plugins -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<script src="plugins/bootstrap/bootstrap.min.js"></script>
-
-<!-- Main Script -->
-<script src="js/script.js"></script>
-
-</body>
-</html>
+<?php include 'includes/footer.php'?>
